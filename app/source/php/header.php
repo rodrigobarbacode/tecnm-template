d678bcad21fd6872359c0d538087ffe890edfa5f
Luis Rodrigo Barba Navarro <78876540+LuisRodrigoBarbaNavarro@users.noreply.github.com>
2024-05-30 07:54:27 -0700

    Delete: App Directory

--- app/source/php/header.php
+++
@@ -1,7 +0,0 @@
-<div class="row">
-    <div class="col-sm-12  text-center">
-        <a href="https://www.gob.mx/" target="_blank" style="z-index:0">
-            <img src="/public/img/tecnmgob/pleca-gobn.png" alt="" class="pleca_gob">
-        </a>
-    </div>
-</div>
