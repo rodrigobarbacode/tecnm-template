b404d6d4df0b786cf32f5795c9d319669902e04b
Luis Rodrigo Barba Navarro <78876540+LuisRodrigoBarbaNavarro@users.noreply.github.com>
2024-06-03 12:11:20 -0700

    Update: Blue Navbar Left Elements

--- app/views/php/main/navbar_white.php
+++ app/views/php/main/navbar_white.php
@@ -1,16 +1,6 @@
-<nav class="navbar navbar-expand-xl navbar-light bg-white" id="main_navbar2">
-    <form action='Buscar' class="navbar-brand ml-1" method='POST'>
-        <input class="w-100 h-100 m-1 border align-items-center form-control" type="text" name='buscar' placeholder="Buscar" autocomplete='off' />
-        <input type="hidden" name="btn_busqueda" />
-    </form>
-    
-    <button class="navbar-toggler border-0" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
-        aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span> Menú
-    </button>
-
-    <div class="collapse navbar-collapse pl-5 pl-lx-0" id="navbarSupportedContent">
-        <ul class="navbar-nav ml-auto">
+<nav class="navbar navbar-expand-xl navbar-light bg-white w-100" id="main_navbar2">
+    <div class="collapse navbar-collapse pl-lx-0 align-center" id="navbarSupportedContent" style="font-weight: bolder;">
+        <ul class="navbar-nav navbar-blue">
             
             <li class="nav-item dropdown">
                 <a class="nav-link active dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
