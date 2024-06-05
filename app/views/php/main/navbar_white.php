3d29ee14167e837750efcdba00adc906ac78322e
Johan Gurrola <pootissonic1808@gmail.com>
2024-06-05 08:45:14 -0700

    Update: Work on White NavBar

--- app/views/php/main/navbar_white.php
+++ app/views/php/main/navbar_white.php
@@ -4,27 +4,54 @@
             
             <li class="nav-item dropdown">
                 <a class="nav-link active dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                    Estudiantes
+                    Alumnos
                 </a>
                 <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
                     <li class="nav-item dropdown">
                         <a class="dropdown-item" href="https://www.tecnm.mx/normateca/Direcci%C3%B3n%20de%20Asuntos%20Escolares%20y%20Apoyo%20a%20Estudiantes/Reglamento_de_Estudiantes_del_TecNM.pdf"> 
-                            Nuevo Reglamento del Estudiante 
+                            Reglamento del Estudiante 
                         </a>
                     </li>
-                    <li class="nav-item dropdown">
-                        <a class="dropdown-item" href="https://financieros.itmexicali.edu.mx/pagos"> 
-                            Líneas de Pago Nuevo Ingreso y Reinscripción
-                        </a>
-                    </li>
+                    
                     <li class="nav-item dropdown">
                         <a class="dropdown-item" href="https://drive.google.com/drive/folders/1uVIh7qyC6-1x-r9dHDUcVZlCWVcBTHTE?usp=sharing">
                             Repositorio de Formatos
                         </a>
                     </li>
+
                     <li class="nav-item dropdown">
-                        <a class="dropdown-item" href="https://sites.google.com/itmexicali.edu.mx/dac/inicio">
-                            Desarrollo Académico
+                        <a class="dropdown-item" href="https://financieros.itmexicali.edu.mx/pagos"> 
+                            Líneas de Pago Nuevo Ingreso y Reinscripción
+                        </a>
+                    </li>
+
+                    <li class="nav-item dropdown">
+                        <a class="dropdown-item" href="./departamentos/Residencia/residencias.html" target="_blank">
+                            Residencia Profesional
+                        </a>
+                    </li>
+
+                    <li class="nav-item dropdown">
+                        <a class="dropdown-item" href="./departamentos/ss/ss.html" target="_blank">
+                            Servicio Social
+                        </a>
+                    </li>
+                    
+                    <li class="nav-item dropdown">
+                        <a class="dropdown-item" href="./documentos/2024/APORTACIONES2024.pdf" target="_blank">
+                            Listado de Aportaciones
+                        </a>
+                    </li>
+
+                    <li class="nav-item dropdown">
+                        <a class="dropdown-item" href="https://docs.google.com/document/d/1LE4YrtseJwhqbscTY2BnLlIHsigLysxh/edit?usp=sharing&ouid=109390354595918364742&rtpof=true&sd=true" target="_blank">
+                            Listado de Actividades Complementarias
+                        </a>
+                    </li>
+                    
+                    <li class="nav-item dropdown">
+                        <a class="dropdown-item" href="./actividades_complementarias/" target="_blank">
+                            Créditos Complementarios  
                         </a>
                     </li>
                 </ul>
