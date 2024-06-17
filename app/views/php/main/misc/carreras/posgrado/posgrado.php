0c20c7c6104b924815afc34f0859358dd2b82260
Johan Gurrola <pootissonic1808@gmail.com>
2024-06-17 11:49:09 -0700

    Update: posgrado.php

--- app/views/php/main/misc/carreras/posgrado/posgrado.php
+++ app/views/php/main/misc/carreras/posgrado/posgrado.php
@@ -11,53 +11,97 @@
 
             <div id="sidebar" class="col-md-12 d-sm-none d-md-none d-lg-none d-xl-block">  
                 <div class="sidebar-separator item-style">
-                    <span>Example Separator</span>
+                    <span>Main Menu</span>
                 </div>
                 
                 <div class="item-style">
-                    <a href="#" onclick="contentToggle('main-title')">
-                    <img width="30" height="30" src="https://img.icons8.com/ios-glyphs/100/FFFFFF/user--v1.png" alt="user--v1"/>        
-                        Opcion 1
+                    <a href="#" onclick="contentToggle('inicio')">
+                    <img width="30" height="30" src="https://img.icons8.com/ios-filled/50/FFFFFF/home.png" alt="home--v1"/>        
+                        Inicio
                     </a>
                 </div>
                 
                 <div class="item-style">
-                    <a href="#" onclick="contentToggle('content1')">
-                    <img width="30" height="30" src="https://img.icons8.com/pastel-glyph/64/FFFFFF/box--v1.png" alt="box--v1"/>
-                        Opcion 2
+                    <a href="#" onclick="toggleSubmenu('personal')">
+                    <img width="30" height="30" src="https://img.icons8.com/ios-glyphs/100/FFFFFF/user--v1.png" alt="user--v1"/>
+                        Personal
+                    <img id="personal" class="submenu-button" width="20" height="20" src="https://img.icons8.com/ios/50/FFFFFF/expand-arrow--v1.png" alt="expand-arrow--v1"/>
+                    </a>
+                </div>
+
+                <div class="submenu personal item-style d-none">
+                    <a href="#" onclick="contentToggle('docente')">
+                    <img width="30" height="30" src="https://img.icons8.com/external-basicons-solid-edtgraphics/50/FFFFFF/external-apple-fruits-basicons-solid-edtgraphics.png" alt="user--v1"/>        
+                        Docente
+                    </a>
+                </div>
+
+                <div class="submenu personal item-style d-none">
+                    <a href="#" onclick="contentToggle('womp')">
+                    <img width="30" height="30" src="https://img.icons8.com/pastel-glyph/64/FFFFFF/books.png" alt="user--v1"/>        
+                        Estudiantes
+                    </a>
+                </div>
+
+                <div class="item-style">
+                    <a href="#" onclick="toggleSubmenu('programas')">
+                    <img width="30" height="30" src="https://img.icons8.com/ios-filled/50/FFFFFF/document--v1.png" alt="document"/>
+                        Programas
+                    <img id="programas" class="submenu-button" width="20" height="20" src="https://img.icons8.com/ios/50/FFFFFF/expand-arrow--v1.png" alt="expand-arrow--v1"/>
+                    </a>
+                </div>
+
+                <div class="submenu programas item-style d-none">
+                    <a href="#" onclick="contentToggle('womp')">
+                    <img width="30" height="30" src="https://img.icons8.com/ios-glyphs/30/FFFFFF/program.png" alt="program"/>        
+                        Maestría en Sistemas Computacionales
+                    </a>
+                </div>
+
+                <div class="submenu programas item-style d-none">
+                    <a href="#" onclick="contentToggle('womp')">
+                    <img width="30" height="30" src="https://img.icons8.com/ios-filled/50/FFFFFF/circuit.png" alt="user--v1"/>        
+                        Maestría en Ingenieria en Electrónica
+                    </a>
+                </div>
+
+                <div class="item-style">
+                    <a href="#" onclick="toggleSubmenu('investigacion')">
+                    <img width="30" height="30" src="https://img.icons8.com/ios-filled/50/FFFFFF/microscope.png" alt="user--v1"/>        
+                        Investigación
+                    <img id="investigacion" class="submenu-button" width="20" height="20" src="https://img.icons8.com/ios/50/FFFFFF/expand-arrow--v1.png" alt="expand-arrow--v1"/>
+                    </a>
+                </div>
+
+                <div class="submenu investigacion item-style d-none">
+                    <a href="#" onclick="contentToggle('womp')">
+                    <img width="30" height="30" src="https://img.icons8.com/ios-filled/50/FFFFFF/groups.png" alt="usergroup"/>        
+                        Cuerpo Académico Informática Industrial
                     </a>
                 </div>
 
                 <div class="sidebar-separator item-style">
-                    <span>Example Separator</span>
-                </div>
-
-                <div class="item-style">
-                    <a href="#" onclick="toggleSubmenu('womp')">
-                    <img width="30" height="30" src="https://img.icons8.com/ios-glyphs/100/FFFFFF/user--v1.png" alt="user--v1"/>        
-                        Opcion 1
-                    <img id="womp" class="submenu-button" width="20" height="20" src="https://img.icons8.com/ios/50/FFFFFF/expand-arrow--v1.png" alt="expand-arrow--v1"/>
-                    </a>
-                </div>
-
-                <div class="submenu womp item-style d-none">
-                    <a href="#">
-                    <img width="30" height="30" src="https://img.icons8.com/ios-glyphs/30/FFFFFF/fire-element--v1.png" alt="fire-element--v1"/>        
-                        Opcion 1
-                    </a>
-                </div>
-
-                <div class="submenu womp item-style d-none">
-                    <a href="#">
-                    <img width="30" height="30" src="https://img.icons8.com/ios-filled/50/FFFFFF/door.png" alt="door"/>     
-                        Opcion 2
-                    </a>
+                    <span>Ofertas Educativas</span>
                 </div>
                 
                 <div class="item-style">
-                    <a href="#" onclick="contentToggle('content2')">
-                    <img width="30" height="30" src="https://img.icons8.com/ios-filled/50/FFFFFF/water.png" alt="water"/>                        
-                        Opcion 2
+                    <a href="#" onclick="contentToggle('')">
+                    <img width="30" height="30" src="https://img.icons8.com/pastel-glyph/64/FFFFFF/checklist--v1.png" alt="checklist"/>                        
+                        Convocatorias
+                    </a>
+                </div>
+
+                <div class="item-style">
+                    <a href="#" onclick="contentToggle('')">
+                    <img width="30" height="30" src="https://img.icons8.com/ios-filled/50/FFFFFF/saddle-stitched-booklet.png" alt="openbook"/>                        
+                        Código de Conducta
+                    </a>
+                </div>
+
+                <div class="item-style">
+                    <a href="#" onclick="contentToggle('')">
+                    <img width="30" height="30" src="https://img.icons8.com/ios-filled/50/FFFFFF/phone.png" alt="phone"/>                        
+                        Contacto
                     </a>
                 </div>
                 
@@ -76,8 +120,8 @@
             </div>
             <!-- Renglon 'Header' que presenta el nombre del departamento -->
             
-            <!-- Renglon de Contenido -->
-            <div id="content1" class="row yogurt-defase p-2 mt-5 mb-5 m-3">
+            <!-- Inicio -->
+            <div id="inicio" class="row yogurt-defase p-2 mt-5 mb-5 m-3">
                 <!-- img -->
                 <div class="col-md-8 align-center">
                     <img src="/img/carreras/Poster-Maestria-Sistemas.jpeg" width="800px" height="1200px" alt="Convocatoria de Maestria en Sistemas">
@@ -120,12 +164,72 @@
                             </a>
                         </li>
                     </ul>
+
+                    <br><br>
+
+                    <p>
+                    Puedes consultar la Convocatoria de Ingreso 
+                    al programa de Maestría en Sistemas Computacionales 
+                    en la opcion de "Convocatorias"
+                    </p>
                 </div>
                 <!-- img -->
             </div>
-            <!-- Renglon de Contenido -->
+            <!-- Inicio -->
 
-            <!-- Finale -->
+            <div id="docente" class="row yogurt-defase p-2 mt-5 mb-5 m-3">
+
+                <div class="col-md-5 p-4">
+                    <div class="row">
+                        <div class="col-md-4">
+                            <img src="/img/posgrado/personal/jaime_0.jpeg" alt="Jaime Olvera">
+                        </div>
+                        <div class="col-md-8">
+                            <p>
+                                <b>Jaime Olvera González</b> <br>
+                                jaimeolvera@itmexicali.edu.mx <br>
+                                Maestría en sistemas computacionales <br>
+                                Áreas de Interés: Programación especializada y robótica
+                            </p>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-md-5 p-4">
+                    <div class="row">
+                        <div class="col-md-4">
+                            <img src="/img/posgrado/personal/jaime_0.jpeg" alt="Jaime Olvera">
+                        </div>
+                        <div class="col-md-8">
+                            <p>
+                                Jaime Olvera González <br>
+                                jaimeolvera@itmexicali.edu.mx <br>
+                                Maestría en sistemas computacionales <br>
+                                Áreas de Interés: Programación especializada y robótica
+                            </p>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-md-5 p-4">
+                    <div class="row">
+                        <div class="col-md-4">
+                            <img src="/img/posgrado/personal/jaime_0.jpeg" alt="Jaime Olvera">
+                        </div>
+                        <div class="col-md-8">
+                            <p>
+                                Jaime Olvera González <br>
+                                jaimeolvera@itmexicali.edu.mx <br>
+                                Maestría en sistemas computacionales <br>
+                                Áreas de Interés: Programación especializada y robótica
+                            </p>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+
+            <!-- Finale 
             <div id="content2" class="row yogurt-defase m-3 p-2">
                 <div class="col-md-8 mx-auto" style="text-align: center;">
                     <h2>Lorem ipsum dolor sit amet consectetur adipisicing elit.:</h2>
@@ -142,7 +246,7 @@
                 </div>
                 
             </div>
-            <!-- Finale -->
+            Finale -->
         </div>
         <!-- Content -->
 
