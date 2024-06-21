685e82cb49d52c96204809336752a45c2572fc8c
Luis Rodrigo Barba Navarro <a20490687@itmexicali.edu.mx>
2024-06-20 22:11:45 -0700

    Insert: Google API Folder

--- app/app.js
+++ app/app.js
@@ -18,8 +18,8 @@ const SCOPES = ['https://www.googleapis.com/auth/calendar.readonly'];
 // The file token.json stores the user's access and refresh tokens, and is
 // created automatically when the authorization flow completes for the first
 // time.
-const TOKEN_PATH = path.join(process.cwd(), 'token.json');
-const CREDENTIALS_PATH = path.join(process.cwd(), 'credentials.json');
+const TOKEN_PATH = path.join(process.cwd(), 'google_api/token.json');
+const CREDENTIALS_PATH = path.join(process.cwd(), 'google_api/credentials.json');
 // Provide the required configuration.
 
 // Google Calendar API Settings.
