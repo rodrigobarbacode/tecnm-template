a0cd3b00e26755c7d5b662d83ef561c4c4984a74
Luis Rodrigo Barba Navarro <a20490687@itmexicali.edu.mx>
2024-06-20 23:45:30 -0700

    Update: Annexes Changed, Swiffy Slider

--- app/views/php/main/annexes.php
+++ app/views/php/main/annexes.php
@@ -3,7 +3,7 @@
     function fetchLocalJSON(file, callback) {
         var xhr = new XMLHttpRequest();
         xhr.overrideMimeType("application/json");
-        xhr.onreadystatechange = function () {
+        xhr.onreadystatechange = function() {
             if (xhr.readyState === 4 && xhr.status === 200) {
                 callback(JSON.parse(xhr.responseText));
             }
@@ -13,32 +13,41 @@
     }
 
     // Get annexes data from JSON file to display them.
-    fetchLocalJSON("json/annexes-list.json", function (data) {
-        var annexes = document.querySelector(".annexes-outer");
+    fetchLocalJSON("json/annexes-list.json", function(data) {
+        var sliderContainer = document.querySelector(".slider-container-annexes");
         var html = "";
 
-        data.forEach(function (annex) {
-            html += "<div class='card' style='height: 6rem; margin: 1rem; border-radius: 1rem;'>";
-            html += "<a target='_blank' href='" + annex.link + "'>";
-            html += "<img loading='lazy' src='" + annex.image + "' style='height: 6rem; padding: 10px'>";
+        data.forEach(function(slide) {
+            html += "<div class='swiffy-slider-item'>";
+            html += "<div class='card' style='width: auto; height: 6rem; margin: 1rem; border-radius: 1rem; display: flex; align-items: center; justify-content: center; overflow: hidden;'>";
+            html += "<a target='_blank' href='" + slide.link + "'>";
+            html += "<img loading='lazy' src='" + slide.image + "' style='max-height: 100%; max-width: 100%; object-fit: contain; border-radius: 1rem;'>";
             html += "</a>";
+            html += "</div>";
             html += "</div>";
         });
 
-        annexes.innerHTML = html;
+        sliderContainer.innerHTML = html;
+
+        // Inicializar Swiffy Slider
+        new SwiffySlider(document.querySelector('.swiffy-slider-annexes'));
     });
-
-
 </script>
 
 <!-- Anexos -->
 <section class="neomorphic-defase ml-3 mr-3 mt-4">
     <div class='row w-100 justify-content-center pt-3 m-0'>
-    <h2 style="color: white; font-size: 4vh; padding-top:15px;"><span>
-        <i class="fa-solid fa-circle-info" style="color: #fab005; padding-right: 2vh;"></i></span>Anexos</h2>
+        <h2 style="color: white; font-size: 4vh; padding-top:15px;"><span>
+                <i class="fa-solid fa-circle-info" style="color: #fab005; padding-right: 2vh;"></i></span>Anexos</h2>
     </div>
     <div class='row w-100 justify-content-center py-3'>
-        <section class='anexos annexes-outer pb-2 anexo sitios'></section>
+        <div class="w-50 swiffy-slider swiffy-slider-annexes slider-nav-autoplay slider-nav-visible slider-nav-chevron slider-item-show4">
+            <div class="slider-container slider-container-annexes">
+                <!-- Los elementos generados por JavaScript se insertarán aquí -->
+            </div>
+            <button type="button" class="slider-nav" aria-label="Go to previous" style="margin-left: -3rem;"></button>
+            <button type="button" class="slider-nav slider-nav-next" aria-label="Go to next" style="margin-right: -3rem;"></button>
+        </div>
     </div>
 </section>
 <br />
