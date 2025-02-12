2e9fafd35c384778195a1cb8c16c90511bdad6fc
Luis Rodrigo Barba Navarro <luisrodrigobarba.work@outlook.com>
2025-02-12 12:08:16 -0800

    feat(app): redo express static path

--- app/app.js
+++ app/app.js
@@ -10,9 +10,6 @@ const express = require("express");
 const api = require("./api");
 const routes = require("./routes");
 
-// Get static files configuration.
-const configureStaticFiles = require("./config/static-files");
-
 // Create the express app.
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -20,8 +17,8 @@ const PORT = process.env.PORT || 3000;
 // Set EJS as the view engine.
 app.set("view engine", "ejs");
 
-// Configure static files.
-configureStaticFiles(app);
+// Use static folder to serve static files.
+app.use(express.static(path.join(__dirname, "public")));
 
 // Load the routes.
 app.use("/", routes);
